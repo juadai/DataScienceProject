21ca2c8b7abca76ef6fa99dc5ad09a6df1a32e15
Nick Jolly <nickjolly90@gmail.com>
2022-06-18 12:40:38 +1000

    Started investigating neg. tree growth and understorey species

---
+++ preprocess data/understorey/species.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+s1 = pd.read_csv('./species.csv')
+
+df = pd.read_excel('./Flora and Fauna species 2007.xls')
+s2 = list(df['Scientific Name'])
+
+i = 0
+for s in list(s1['x'].values):
+  if s not in s2:
+    print(s)
+    i += 1
+
+print(i)
+
