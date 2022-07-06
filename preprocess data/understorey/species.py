e271f76a2c7b298c1ae29ad0670e1088d11943c3
Nick Jolly <nickjolly90@gmail.com>
2022-07-07 09:02:21 +1000

    Upload additional tree data including re-mapping tree numbers

--- preprocess data/understorey/species.py
+++ preprocess data/understorey/species.py
@@ -1,15 +1,29 @@
 import pandas as pd
+import nltk
 
 s1 = pd.read_csv('./species.csv')
 
 df = pd.read_excel('./Flora and Fauna species 2007.xls')
 s2 = list(df['Scientific Name'])
+s3 = [s.lower() for s in s2]
+
+not_matched = []
 
 i = 0
 for s in list(s1['x'].values):
-  if s not in s2:
-    print(s)
+  if s.lower() not in s3:
+    not_matched.append(s)
     i += 1
 
-print(i, "out of", len(s1), "species to be matched")
+print(i, "out of", len(s1), "species still to be matched")
 
+
+j = 0
+for word in not_matched:
+  scores = []
+  for s in s2:
+    score = nltk.edit_distance(word.lower(), s)
+    scores.append((score, s))
+    if score <= 3:
+      print(word, sorted(scores)[0])
+      j += 1
