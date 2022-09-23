60ab658e56f4af54f43c4ab04861e789b703da14
mirrokle <curtaincall1025@gmail.com>
2022-09-23 11:44:04 +1000

    excel data parsing init

--- Extra/data_parsing.py
+++ Extra/data_parsing.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def read(filename):
     df = pd.read_excel(filename, sheet_name=None, engine="openpyxl")
-    for name, sheet in df.items():
+    for name, sheet in df.items(): # iterate over all sheets and contents
         print(name)
         print(sheet.head(3))
 
