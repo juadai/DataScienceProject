2fba9f2aea03822198097c4999a437ca550124af
mirrokle <curtaincall1025@gmail.com>
2022-10-05 14:46:10 +1100

    extra task - updated folder structure

--- Extra/data_parsing.py
+++ Extra/data_parsing.py
@@ -1,9 +1,18 @@
+import os
 import pandas as pd
 
 def read(filename):
-    df = pd.read_excel(filename, sheet_name=None, engine="openpyxl")
-    for name, sheet in df.items(): # iterate over all sheets and contents
-        print(name)
-        print(sheet.head(3))
+    df = pd.read_excel(filename, sheet_name="Final Deliverable", engine="openpyxl")
+    print(df.head(3))
 
-read('Gannawarra.xlsx')
+
+def main():
+    list_subfolders_paths = [f.path for f in os.scandir('.') if f.is_dir()]
+    print(list_subfolders_paths)
+
+    for path in list_subfolders_paths:
+        for f in os.scandir(path):
+            if f.is_file():
+                read(f)
+
+main()
