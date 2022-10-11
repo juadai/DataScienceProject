4134964ee280157ac2e2dde1705b72b61ab99c28
Nick Jolly <nickjolly90@gmail.com>
2022-10-11 12:16:34 +1100

    Expanded on tree model visualisations

--- Tree DBH/tree_model_interval_visualisations.R
+++ Tree DBH/tree_model_interval_visualisations.R
@@ -20,25 +20,67 @@ DBHdatarm$InGap = factor(DBHdatarm$InGap)
 
 mixed2.0 = lmer(log(DBH.year.6)~log(DBH.year.0)+Treatment+InGap+log(DBH.year.0):InGap+(1|Plot), data = DBHdatarm)
 
-X <- data.frame('DBH.year.0'=1:100,
-                'InGap'=FALSE,
-                'Plot'=6,
-                'Treatment'='Control')
 
+
+# Predict Control
+X <- data.frame('DBH.year.0'=1:45,
+                'InGap'=FALSE,
+                'Plot'=3,
+                'Treatment'='Control')
 
 PI <- predictInterval(merMod = mixed2.0, newdata = X,
                       level = 0.95, n.sims = 1000,
                       stat = "median", type="linear.prediction",
                       include.resid.var = TRUE)
 
-plot(1:100, exp(PI$fit), type='l',
-     xlim=c(20,60))
-lines(1:100, exp(PI$upr), type='l', col='grey')
-lines(1:100, exp(PI$lwr), type='l', col='grey')
+# Plot Control vs. T1 (Not large trees)
+X_T1 <- data.frame('DBH.year.0'=1:45,
+                'InGap'=FALSE,
+                'Plot'=3,
+                'Treatment'='T1: Gap')
 
-X_ingap <- data.frame('DBH.year.0'=1:100,
+PI_2 <- predictInterval(merMod = mixed2.0, newdata = X_T1,
+                        level = 0.95, n.sims = 1000,
+                        stat = "median", type="linear.prediction",
+                        include.resid.var = TRUE)
+
+plot(0, xlim=c(30,50), ylim=c(30,80))
+lines(1:45, exp(PI$fit), type='l')
+lines(1:45, exp(PI$upr), type='l', col='grey')
+lines(1:45, exp(PI$lwr), type='l', col='grey')
+
+lines(1:45, exp(PI_2$fit), type='l', col='red', cex=4)
+lines(1:45, exp(PI_2$upr), type='l', lty='dashed', col='red')
+lines(1:45, exp(PI_2$lwr), type='l', lty='dashed', col='red')
+abline(v=45, lty='dashed')
+
+# Plot control vs T2 (Not large trees)
+X_T2 <- data.frame('DBH.year.0'=1:45,
+                   'InGap'=FALSE,
+                   'Plot'=3,
+                   'Treatment'='T2: Radial')
+
+PI_2 <- predictInterval(merMod = mixed2.0, newdata = X_T1,
+                        level = 0.95, n.sims = 1000,
+                        stat = "median", type="linear.prediction",
+                        include.resid.var = TRUE)
+
+plot(0, xlim=c(30,50), ylim=c(30,80))
+lines(1:45, exp(PI$fit), type='l')
+lines(1:45, exp(PI$upr), type='l', col='grey')
+lines(1:45, exp(PI$lwr), type='l', col='grey')
+
+lines(1:45, exp(PI_2$fit), type='l', col='red', cex=4)
+lines(1:45, exp(PI_2$upr), type='l', lty='dashed', col='red')
+lines(1:45, exp(PI_2$lwr), type='l', lty='dashed', col='red')
+abline(v=45, lty='dashed')
+
+
+# Plot control vs. in-gap
+
+X_ingap <- data.frame('DBH.year.0'=33:70,
                       'InGap'=TRUE,
-                      'Plot'=6,
+                      'Plot'=3,
                       'Treatment'='T2: Radial')
 
 PI_2 <- predictInterval(merMod = mixed2.0, newdata = X_ingap,
@@ -46,8 +88,22 @@ PI_2 <- predictInterval(merMod = mixed2.0, newdata = X_ingap,
                               stat = "median", type="linear.prediction",
                               include.resid.var = TRUE)
 
+plot(0, xlim=c(30,50), ylim=c(30,80))
+lines(1:45, exp(PI$fit), type='l')
+lines(1:45, exp(PI$upr), type='l', col='grey')
+lines(1:45, exp(PI$lwr), type='l', col='grey')
 
-lines(1:100, exp(PI_2$fit), type='l', col='red')
-lines(1:100, exp(PI_2$upr), type='l', lty='dashed', col='red')
-lines(1:100, exp(PI_2$lwr), type='l', lty='dashed', col='red')
+lines(33:70, exp(PI_2$fit), type='l', col='red', cex=4)
+lines(33:70, exp(PI_2$upr), type='l', lty='dashed', col='red')
+lines(33:70, exp(PI_2$lwr), type='l', lty='dashed', col='red')
+abline(v=33, lty='dashed')
+abline(v=45, lty='dashed')
       
+summary(DBHdatarm[DBHdatarm$Large=='Yes', 'DBH.year.6'])
+summary(DBHdatarm[DBHdatarm$Large=='No', 'DBH.year.6'])
+
+alpha <- coef(mixed2.0)$Plot[,'(Intercept)']
+summary(alpha)
+exp(coef(mixed2.0)$Plot)
+
+bootMer()
