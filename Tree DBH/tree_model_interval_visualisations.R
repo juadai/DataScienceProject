c9130613ed5782bf5260be8f402033d4865a9d0d
Nick Jolly <nickjolly90@gmail.com>
2022-10-11 13:21:01 +1100

    Expanded on tree model visualisations

--- Tree DBH/tree_model_interval_visualisations.R
+++ Tree DBH/tree_model_interval_visualisations.R
@@ -22,8 +22,8 @@ mixed2.0 = lmer(log(DBH.year.6)~log(DBH.year.0)+Treatment+InGap+log(DBH.year.0):
 
 
 
-# Predict Control
-X <- data.frame('DBH.year.0'=1:45,
+# Fitted values for Control
+X <- data.frame('DBH.year.0'=1:60,
                 'InGap'=FALSE,
                 'Plot'=3,
                 'Treatment'='Control')
@@ -34,46 +34,40 @@ PI <- predictInterval(merMod = mixed2.0, newdata = X,
                       include.resid.var = TRUE)
 
 # Plot Control vs. T1 (Not large trees)
-X_T1 <- data.frame('DBH.year.0'=1:45,
+X_T1 <- data.frame('DBH.year.0'=1:60,
                 'InGap'=FALSE,
                 'Plot'=3,
                 'Treatment'='T1: Gap')
 
-PI_2 <- predictInterval(merMod = mixed2.0, newdata = X_T1,
+PI_T1 <- predictInterval(merMod = mixed2.0, newdata = X_T1,
                         level = 0.95, n.sims = 1000,
                         stat = "median", type="linear.prediction",
                         include.resid.var = TRUE)
 
-plot(0, xlim=c(30,50), ylim=c(30,80))
-lines(1:45, exp(PI$fit), type='l')
-lines(1:45, exp(PI$upr), type='l', col='grey')
-lines(1:45, exp(PI$lwr), type='l', col='grey')
 
-lines(1:45, exp(PI_2$fit), type='l', col='red', cex=4)
-lines(1:45, exp(PI_2$upr), type='l', lty='dashed', col='red')
-lines(1:45, exp(PI_2$lwr), type='l', lty='dashed', col='red')
-abline(v=45, lty='dashed')
+plot(0, xlim=c(30,50), ylim=c(30,60), main = 'Control vs T1, T2',
+     xlab= 'DBH at Year 0', ylab='Fitted value for DBH at Year 6')
+lines(1:60, exp(PI$fit), type='l')
+lines(1:60, exp(PI$upr), type='l', col='grey')
+lines(1:60, exp(PI$lwr), type='l', col='grey')
 
-# Plot control vs T2 (Not large trees)
-X_T2 <- data.frame('DBH.year.0'=1:45,
+lines(1:60, exp(PI_T1$fit), type='l', col='red', cex=4)
+lines(1:60, exp(PI_T1$upr), type='l', lty='dashed', col='red')
+lines(1:60, exp(PI_T1$lwr), type='l', lty='dashed', col='red')
+
+X_T2 <- data.frame('DBH.year.0'=1:60,
                    'InGap'=FALSE,
                    'Plot'=3,
                    'Treatment'='T2: Radial')
 
-PI_2 <- predictInterval(merMod = mixed2.0, newdata = X_T1,
+PI_T2 <- predictInterval(merMod = mixed2.0, newdata = X_T1,
                         level = 0.95, n.sims = 1000,
                         stat = "median", type="linear.prediction",
                         include.resid.var = TRUE)
 
-plot(0, xlim=c(30,50), ylim=c(30,80))
-lines(1:45, exp(PI$fit), type='l')
-lines(1:45, exp(PI$upr), type='l', col='grey')
-lines(1:45, exp(PI$lwr), type='l', col='grey')
-
-lines(1:45, exp(PI_2$fit), type='l', col='red', cex=4)
-lines(1:45, exp(PI_2$upr), type='l', lty='dashed', col='red')
-lines(1:45, exp(PI_2$lwr), type='l', lty='dashed', col='red')
-abline(v=45, lty='dashed')
+lines(1:60, exp(PI_T2$fit), type='l', col='green', cex=4)
+lines(1:60, exp(PI_T2$upr), type='l', lty='dashed', col='green')
+lines(1:60, exp(PI_T2$lwr), type='l', lty='dashed', col='green')
 
 
 # Plot control vs. in-gap
@@ -83,21 +77,23 @@ X_ingap <- data.frame('DBH.year.0'=33:70,
                       'Plot'=3,
                       'Treatment'='T2: Radial')
 
-PI_2 <- predictInterval(merMod = mixed2.0, newdata = X_ingap,
+PI_ingap <- predictInterval(merMod = mixed2.0, newdata = X_ingap,
                               level = 0.95, n.sims = 1000,
                               stat = "median", type="linear.prediction",
                               include.resid.var = TRUE)
 
-plot(0, xlim=c(30,50), ylim=c(30,80))
-lines(1:45, exp(PI$fit), type='l')
-lines(1:45, exp(PI$upr), type='l', col='grey')
-lines(1:45, exp(PI$lwr), type='l', col='grey')
+plot(0, xlim=c(30,50), ylim=c(30,60), main='Control vs In Gap',
+     
+     xlab= 'DBH at Year 0', ylab='Fitted value for DBH at Year 6')
+lines(1:60, exp(PI$fit), type='l')
+lines(1:60, exp(PI$upr), type='l', col='grey')
+lines(1:60, exp(PI$lwr), type='l', col='grey')
 
-lines(33:70, exp(PI_2$fit), type='l', col='red', cex=4)
-lines(33:70, exp(PI_2$upr), type='l', lty='dashed', col='red')
-lines(33:70, exp(PI_2$lwr), type='l', lty='dashed', col='red')
+lines(33:70, exp(PI_ingap$fit)
+      , type='l', col='red', cex=4)
+lines(33:70, exp(PI_ingap$upr), type='l', lty='dashed', col='red')
+lines(33:70, exp(PI_ingap$lwr), type='l', lty='dashed', col='red')
 abline(v=33, lty='dashed')
-abline(v=45, lty='dashed')
       
 summary(DBHdatarm[DBHdatarm$Large=='Yes', 'DBH.year.6'])
 summary(DBHdatarm[DBHdatarm$Large=='No', 'DBH.year.6'])
